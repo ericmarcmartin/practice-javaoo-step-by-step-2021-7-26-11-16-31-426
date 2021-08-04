6579047ff4c8319dbcb9622afab3bee3955317d3
martier <martier@oocl.com>
2021-08-05 00:50:54 +0800

    [practice06][Eric] feat: add Teacher.java

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,48 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+    private final String TEACH = "teach ";
+    String message = super.introduce() + " I am a Teacher. I ";
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klassDoesNotMatchWithTeacher()) {
+            Klass klass = new Klass(0);
+            return doNotTeach(klass);
+        }
+        return doTeach(klass.getDisplayName());
+    }
+
+    private boolean klassDoesNotMatchWithTeacher() {
+        return klass == null;
+    }
+
+    private String doTeach(String displayName) {
+        return message + TEACH + displayName + ".";
+    }
+
+    private String doNotTeach(Klass klass) {
+        return message + TEACH + klass.getDisplayName();
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().getNumber() != klass.getNumber()) {
+            return message + "don't " + TEACH + student.getName() + ".";
+        }
+        return doTeach(student.getName());
+    }
 }
