f04c0c3b64302c723aaae3bf01502fda163b1bf3
martier <martier@oocl.com>
2021-08-04 23:27:35 +0800

    [practice05][Eric] feat: added Person, Student, Teacher

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,27 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass = 0;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String tom, int age) {
+        super(tom, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String message = super.introduce() + " I am a Teacher. I teach ";
+        if (klass == 0) {
+            return message + String.format("No Class.");
+        }
+        return message + String.format("Class %d.", klass);
+    }
 }
