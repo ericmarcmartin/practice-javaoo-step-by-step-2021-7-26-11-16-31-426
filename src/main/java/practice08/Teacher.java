bb31efbb3e2cea36303370155a1f77c91ea56435
martier <martier@oocl.com>
2021-08-07 17:14:34 +0800

    [practice 08][Eric] feat: add implementation to should_teacher_introduce_itself_with_which_class_it_teaches

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,7 +1,8 @@
 package practice08;
 
-public class Teacher extends Person{
+public class Teacher extends Person {
     private Klass klass;
+
     public Teacher(int id, String name, int age, Klass klass) {
         super(id, name, age);
         this.klass = klass;
@@ -9,5 +10,10 @@ public class Teacher extends Person{
 
     public Klass getKlass() {
         return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a Teacher. I teach Class %d.", super.introduce(), klass.getNumber());
     }
 }
