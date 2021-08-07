1bbca643f9ec2acd1a2e0890dfb517e03248dbf5
martier <martier@oocl.com>
2021-08-07 16:14:32 +0800

    [practice 08][Eric] test: add should_person_have_id_name_and_age

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,7 @@
 package practice08;
 
 public class Klass {
+    public Klass(int number) {
+
+    }
 }
--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,21 @@
 package practice08;
 
 public class Person {
+    private final int id;
+    private final String name;
+    private final int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public String getAge() {
+        return null;
+    }
 }
