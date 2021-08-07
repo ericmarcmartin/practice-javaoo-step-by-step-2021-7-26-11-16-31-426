84c209b36b8a16732a2350e1e8665c8865428d73
martier <martier@oocl.com>
2021-08-07 15:34:12 +0800

    [practice 07][Eric] feat: add implementation to should_teacher_introduce_itself_with_no_class_teaching

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,10 +1,14 @@
 package practice07;
 
 public class Teacher extends Person {
-    private final Klass klass;
+    private Klass klass;
     public Teacher(int id, String name, int age, Klass klass) {
         super(id, name, age);
         this.klass = klass;
+    }
+
+    public Teacher(int id, String teacher, int age) {
+        super(id, teacher, age);
     }
 
     public Klass getKlass() {
@@ -12,6 +16,9 @@ public class Teacher extends Person {
     }
 
     public String introduce() {
+        if (this.klass == null) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
         return super.introduce() + String.format(" I am a Teacher. I teach %s.", klass.getDisplayName());
     }
 }
