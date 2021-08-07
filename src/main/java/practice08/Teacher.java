a7fe8b01db7a5bb1b3e8a20796a9e1621a882263
martier <martier@oocl.com>
2021-08-07 17:10:20 +0800

    [practice 08][Eric] test: add should_teacher_have_name_and_age_and_class

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,12 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return null;
+    }
 }
