17b1ca144d69ff758a7a533f60ae2781a861f31b
martier <martier@oocl.com>
2021-08-07 14:07:33 +0800

    [practice 07][Eric] feat: add implementation to should_person_with_same_id_be_same_one

--- src/main/java/practice07/Person.java
+++ src/main/java/practice07/Person.java
@@ -1,5 +1,7 @@
 package practice07;
 
+import java.util.Objects;
+
 public class Person {
     private final String name;
     private final int age;
@@ -22,5 +24,13 @@ public class Person {
 
     public int getKlass() {
         return klass;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && klass == person.klass && Objects.equals(name, person.name);
     }
 }
