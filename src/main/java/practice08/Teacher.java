f1fd00266b9680786cb9d0ea9228c397cdd0cc0b
martier <martier@oocl.com>
2021-08-07 17:10:55 +0800

    [practice 08][Eric] feat: add implementation to should_teacher_have_name_and_age_and_class

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -4,9 +4,10 @@ public class Teacher extends Person{
     private Klass klass;
     public Teacher(int id, String name, int age, Klass klass) {
         super(id, name, age);
+        this.klass = klass;
     }
 
     public Klass getKlass() {
-        return null;
+        return klass;
     }
 }
