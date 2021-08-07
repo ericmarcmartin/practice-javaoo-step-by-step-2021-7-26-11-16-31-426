6ac5469c1e0e6d69a56248fcd350a4f1983c2ed2
martier <martier@oocl.com>
2021-08-07 18:21:24 +0800

    [practice 09][Eric] feat: add implementation to should_teacher_introduce_itself_with_which_classes_it_teaches

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -3,14 +3,37 @@ package practice09;
 import java.util.LinkedList;
 
 public class Teacher extends Person {
-    private LinkedList<Klass> klass;
+    private LinkedList<Klass> classes;
 
-    public Teacher(int id, String name, int age, LinkedList<Klass> klass) {
+    public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
         super(id, name, age);
-        this.klass = klass;
+        this.classes = classes;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
     }
 
     public LinkedList<Klass> getClasses() {
-        return klass;
+        return classes;
     }
+
+    @Override
+    public String introduce() {
+        if(classes.size()>1){
+            String klasses ="";
+            for(Klass klass : classes){
+                if(classes.getFirst().equals(klass)){
+                    klasses = String.format("%s",klass.getNumber());
+                }
+                else{
+                    klasses = String.format("%s, %s",klasses,klass.getNumber());
+                }
+            }
+            return String.format("%s I am a Teacher. I teach Class %s.", super.introduce(), klasses);
+
+        }
+        return String.format("%s I am a Teacher. I teach Class %d.", super.introduce(), classes.getFirst().getNumber());
+    }
+
 }
