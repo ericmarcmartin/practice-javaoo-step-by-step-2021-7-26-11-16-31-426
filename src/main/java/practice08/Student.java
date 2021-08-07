10ecf7f01366bf1859a39160c94532dfcfa26eb1
martier <martier@oocl.com>
2021-08-07 16:52:23 +0800

    [practice 08][Eric] test: add should_student_have_name_age_and_class_number

--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -5,4 +5,8 @@ public class Student extends Person{
     public Student(int id, String name, int age, Klass klass) {
         super(id, name, age);
     }
+
+    public Klass getKlass() {
+        return null;
+    }
 }
