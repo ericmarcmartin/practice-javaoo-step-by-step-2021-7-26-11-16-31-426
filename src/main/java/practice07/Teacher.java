d5c280a809ca8ed3ee61518b64d5703cd864f8d6
martier <martier@oocl.com>
2021-08-07 15:48:01 +0800

    [practice 07][Eric] feat: add implementation to should_teacher_introduce_a_student_it_does_not_teach

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -2,7 +2,7 @@ package practice07;
 
 public class Teacher extends Person {
     private Klass klass;
-    String I_AM_A_TEACHER = "I am a Teacher. I teach";
+    String I_AM_A_TEACHER = "I am a Teacher. I";
 
     public Teacher(int id, String name, int age, Klass klass) {
         super(id, name, age);
@@ -19,11 +19,13 @@ public class Teacher extends Person {
 
     public String introduce() {
         return klass == null ?
-                String.format("%s %s No Class.", super.introduce(), I_AM_A_TEACHER) :
-                String.format("%s %s %s.", super.introduce(), I_AM_A_TEACHER, klass.getDisplayName());
+                String.format("%s %s teach No Class.", super.introduce(), I_AM_A_TEACHER) :
+                String.format("%s %s teach %s.", super.introduce(), I_AM_A_TEACHER, klass.getDisplayName());
     }
 
     public String introduceWith(Student student) {
-        return String.format("%s %s %s.", super.introduce(), I_AM_A_TEACHER, student.getName());
+        return klass.getNumber()==student.getKlass().getNumber() ?
+                String.format("%s %s teach %s.", super.introduce(), I_AM_A_TEACHER, student.getName()) :
+                String.format("%s %s don't teach %s.", super.introduce(), I_AM_A_TEACHER, student.getName());
     }
 }
