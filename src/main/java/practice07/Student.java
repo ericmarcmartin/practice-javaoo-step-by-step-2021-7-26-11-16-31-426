5b14e75e8cda651f51b76810389b8dc3ca80ff41
martier <martier@oocl.com>
2021-08-07 14:55:38 +0800

    [practice 07][Eric] feat: add implementation to should_student_have_name_age_and_class_number

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,11 +1,13 @@
 package practice07;
 
 public class Student extends Person{
+    private final Klass klass;
     public Student(int id, String name, int age, Klass klass) {
         super(id, name, age);
+        this.klass = klass;
     }
 
     public Klass getKlass() {
-        return null;
+        return this.klass;
     }
 }
